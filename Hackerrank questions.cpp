de0a7deef83c568db07512e8250ddce00752de5d
Dev <strangermana1@gmail.com>
2022-11-03 12:10:49 +0530

    Initial Commit

---
+++ Hackerrank questions.cpp
@@ -0,0 +1,87 @@
+// Hacker rank c++
+
+
+// for loop
+/*
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int a,b;
+    
+    cin>>a;
+    cin>>b;
+    for(int n=a;n<=b;n++) {
+        if (n<=9) {
+            if (n == 1) {
+                 cout << "one" << endl;
+             } else if (n == 2) {
+                 cout << "two" << endl;
+            } else if (n == 3) {
+                cout << "three" << endl;
+            } else if (n == 4) {
+                cout << "four" << endl;
+            } else if (n == 5) {
+                cout << "five" << endl;
+            } else if (n == 6) {
+             cout << "six" << endl;
+            } else if (n == 7) {
+                cout <<"seven" << endl;
+             } else if (n == 8) {
+                cout << "eight" << endl;
+             } else if (n == 9) {
+                 cout << "nine" << endl;
+            }
+        }
+    else if(n%2==0) {
+        
+            cout<<"even" << endl;
+        }
+        else{
+            cout<<"odd"<< endl;
+        } 
+    
+    }    
+        
+    
+    
+    }
+    
+ */ 
+
+
+//  function return max value
+/*#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d){
+    if (a>b && a>c && a>d) {
+      return a;
+    }
+    else if (b>a && b>c && b>d) {
+      return b;
+    }
+    else if (c>b && c>a && c>d) {
+      return c;
+    }
+    else (d>a && d>b && d>c); {
+      return d;
+    }
+}
+
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}*/
+    
+
+
+
+
